22c9f33e2ad30da3ee664d12987909c4e69e0380
francisruizlopez <francis.ruizlopez@gmail.com>
2023-01-31 07:55:13 +0100

    primera version clase persona

---
+++ ExamenEGIT/src/Persona.java
@@ -0,0 +1,18 @@
+
+public class Persona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//cambio para generar conflicto
+	
+	}
+	
+	void metodoRama1() {
+		
+	}
+	
+	void metodoRama2() {
+		
+	}
+
+}
